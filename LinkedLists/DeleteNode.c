53f553d79503ab5579a4cd6359b0a00a9ee60ed7
Arun Sunkar <79840092+arun0810@users.noreply.github.com>
2021-12-15 23:58:26 +0530

    Update DeleteNode.c

--- LinkedLists/DeleteNode.c
+++ LinkedLists/DeleteNode.c
@@ -7,4 +7,21 @@ struct node
 {
     int data;
     struct node *link;
+};
+
+int main()
+{
+    struct node *head = NULL;
+    head = malloc(sizeof (struct node));
+    head->data = 5;
+    head->link = NULL;
+
+    struct node *current = malloc(sizeof(struct node));
+    current->data = 40;
+    current->link = NULL;
+    head->link = current;
+
+    
+
+    return 0;
 }
