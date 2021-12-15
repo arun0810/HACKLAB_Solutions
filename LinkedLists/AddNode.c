9ccb5e28feca2f4904a6243f8852fbda800e5116
Arun Sunkar <79840092+arun0810@users.noreply.github.com>
2021-12-15 23:09:48 +0530

    Update AddNode.c

---
+++ LinkedLists/AddNode.c
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+
+//CREATING LINKEDLISTS AND ADDING NODES
+struct node
+{
+    int data;
+    struct node *link;
+};
+
+int main()
+{
+    struct node *head = NULL;
+    head = malloc(sizeof (struct node));
+    head->data = 5;
+    head->link = NULL;
+    printf("%d",head->data);
+    return 0;
+}
