1c620d27745cd9c610066ce09ed15894977b33b4
Arun Sunkar <79840092+arun0810@users.noreply.github.com>
2021-12-16 18:02:40 +0530

    Update sample.c

--- 2. LinkedLists/sample.c
+++ 2. LinkedLists/sample.c
@@ -1,44 +1,155 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node
-{
-    int data;          
-    struct node* link; 
-};
 
-//ADDING NODES
-void add(int n)
-{
-    struct node* head;
-    struct node* current;
-    struct node* previous;
-    int num,i;
-    head = malloc(sizeof(struct node));
-    previous = head;
+/* Structure of a node */
+struct node {
+    int data;          // Data 
+    struct node *next; // Address 
+}*head;
 
-    printf("Node 1: ");
-    scanf("%d",&num);
-    head->data = num;
-    head->link = NULL;
 
-    for(i=2;i<=n;i++)
-    {
-        current = malloc(sizeof(struct node));
-        printf("Node %d:",i);
-        scanf("%d",&num);
-        current->data = num;
-        current->link = NULL;
-        previous->link = current;
-        previous = current;
-    }
-}
+void createList(int n);
+void insertNodeAtEnd(int data);
+void displayList();
+
 
 int main()
 {
-    int n;
-    printf("No of Nodes: ");
-    scanf("%d",&n);
-    add(n);
+    int n, data;
+
+    /*
+     * Create a singly linked list of n nodes
+     */
+    printf("Enter the total number of nodes: ");
+    scanf("%d", &n);
+    createList(n);
+
+    printf("\nData in the list \n");
+    displayList();
+
+    /*
+     * Insert data at the end of the singly linked list
+     */
+    printf("\nEnter data to insert at end of the list: ");
+    scanf("%d", &data);
+    insertNodeAtEnd(data);
+
+    printf("\nData in the list \n");
+    displayList();
+
     return 0;
-}   
+}
+
+
+
+void createList(int n)
+{
+    struct node *newNode, *temp;
+    int data, i;
+
+    head = (struct node *)malloc(sizeof(struct node));
+
+    if(head == NULL)
+    {
+        printf("Unable to allocate memory.");
+    }
+    else
+    {
+        /*
+         * Reads data of node from the user
+         */
+        printf("Enter the data of node 1: ");
+        scanf("%d", &data);
+
+        head->data = data; // Link the data field with data
+        head->next = NULL; // Link the address field to NULL
+
+        temp = head;
+
+        /*
+         * Create n nodes and adds to linked list
+         */
+        for(i=2; i<=n; i++)
+        {
+            newNode = (struct node *)malloc(sizeof(struct node));
+
+            /* If memory is not allocated for newNode */
+            if(newNode == NULL)
+            {
+                printf("Unable to allocate memory.");
+                break;
+            }
+            else
+            {
+                printf("Enter the data of node %d: ", i);
+                scanf("%d", &data);
+
+                newNode->data = data; // Link the data field of newNode with data
+                newNode->next = NULL; // Link the address field of newNode with NULL
+
+                temp->next = newNode; // Link previous node i.e. temp to the newNode
+                temp = temp->next; 
+            }
+        }
+
+        printf("SINGLY LINKED LIST CREATED SUCCESSFULLY\n");
+    }
+}
+
+
+/*
+ * Create a new node and inserts at the end of the linked list.
+ */
+void insertNodeAtEnd(int data)
+{
+    struct node *newNode, *temp;
+
+    newNode = (struct node*)malloc(sizeof(struct node));
+
+    if(newNode == NULL)
+    {
+        printf("Unable to allocate memory.");
+    }
+    else
+    {
+        newNode->data = data; // Link the data part
+        newNode->next = NULL; 
+
+        temp = head;
+
+        // Traverse to the last node
+        while(temp != NULL && temp->next != NULL)
+            temp = temp->next;
+
+        temp->next = newNode; // Link address part
+
+        printf("DATA INSERTED SUCCESSFULLY\n");
+    }
+}
+
+
+/*
+ * Display entire list
+ */
+void displayList()
+{
+    struct node *temp;
+
+    /*
+     * If the list is empty i.e. head = NULL
+     */
+    if(head == NULL)
+    {
+        printf("List is empty.");
+    }
+    else
+    {
+        temp = head;
+        while(temp != NULL)
+        {
+            printf("Data = %d\n", temp->data); // Print data of current node
+            temp = temp->next;                
+        }
+    }
+}
