cba20abe5f7a906c01ba11c98b354aa8cb91949e
Arun Sunkar <79840092+arun0810@users.noreply.github.com>
2021-12-16 15:44:35 +0530

    Create sample.c

---
+++ 2. LinkedLists/sample.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;          
+    struct node* link; 
+};
+
+//ADDING NODES
+void add(int n)
+{
+    struct node* head;
+    struct node* current;
+    struct node* previous;
+    int num,i;
+    head = malloc(sizeof(struct node));
+    previous = head;
+
+    printf("Node 1: ");
+    scanf("%d",&num);
+    head->data = num;
+    head->link = NULL;
+
+    for(i=2;i<=n;i++)
+    {
+        current = malloc(sizeof(struct node));
+        previous->link = current;
+        printf("Node %d:",i);
+        scanf("%d",&num);
+        current->data = num;
+        current->link = NULL;
+    }
+}
+
+int main()
+{
+    int n;
+    printf("No of Nodes: ");
+    scanf("%d",&n);
+    add(n);
+    return 0;
+}   
